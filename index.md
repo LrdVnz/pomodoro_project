e928cededf5bc8514941a9dd6a7cc382651b76b3
Vincenzo <vinzgameslol@gmail.com>
2020-04-15 10:12:10 +0200

    Added Index.md

---
+++ index.md
@@ -0,0 +1,5 @@
+index.html 
+style.css
+script.js
+sharp.mp3
+accomplished.mp3
--- script.js
+++ script.js
@@ -7,11 +7,17 @@ function startTimer() {
 
 let minutes = 25;
 let seconds = 0;
-let sessions = 4; 
+let sessions = 3;
+let breakTime; 
+let startAudio = new Audio('sharp.mp3');
+let endAudio = new Audio('accomplished.mp3');
 
 let btn = document.querySelector("#start");
 
-document.addEventListener("click", btn)
+btn.addEventListener("click", function(){
+    startTimer();
+    startAudio.play();
+})
 
 function myTimer() {
         if(seconds == 60){
@@ -28,12 +34,18 @@ function myTimer() {
     if(!sessions){
         if(minutes == 0 && seconds == 0){
            sessions = 4;
-           minutes = 15;
+           breakTime = 15;
+           minutes = breakTime;
+           let audio = new Audio('accomplished.mp3');
+           endAudio.play();
           }
         }
         else if(minutes == 0 && seconds == 0){
+        let audio = new Audio('accomplished.mp3');
+        endAudio.play();
         sessions--;  
-        minutes = 5 ;
+        breakTime = 5 ;
+        minutes = breakTime;
      }
 }
 
@@ -47,3 +59,27 @@ function resetTimer(){
     myVar = setInterval(myTimer, 1000);
 }
 
+let firstSlider = document.getElementById("firstRange");
+let firstOutput = document.getElementById("timerRange");
+let timer = document.getElementById("timer")
+timer.innerHTML = "25 : 00"
+firstOutput.innerHTML = firstSlider.value + " minutes";
+
+firstSlider.oninput = function(){
+    firstOutput.innerHTML = this.value + " minutes";
+    timer.innerHTML = this.value + " : 00";
+    minutes = this.value;
+}
+
+
+
+let secondSlider = document.getElementById("secondRange");
+let secondOutput = document.getElementById("breakRange");
+let breakTimer = document.getElementById("breakTimer")
+timer.innerHTML = "25 : 00"
+secondOutput.innerHTML = secondSlider.value + " minutes";
+
+secondSlider.oninput = function(){
+    secondOutput.innerHTML = this.value + " minutes";
+    breakTime = this.value;
+}
